643f4b8bc0e6a5dc171be37b52d8b8a1eb0a0e19
Lauren-Nicole <popich.red@gmail.com>
2022-08-12 14:10:33 +0000

    Import bootstrap form. Reassign elements.

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,12 +1,33 @@
 import React from "react";
+import { Container, Form, Button } from "react-bootstrap";
 // import styles from "../../styles/SignUpForm.module.css";
 
 const SignUpForm = () => {
 
     return (
-        <div>
-            <h1>WHAT?!</h1>
-        </div>
+        <Container>
+            <h1>WHAT?!?!</h1>
+            <Form>
+                <Form.Group controlId="username">
+                    <Form.Label>username</Form.Label>
+                    <Form.Control type="text" placeholder="username" name="username" />
+                </Form.Group>
+
+                <Form.Group controlId="password">
+                    <Form.Label>password</Form.Label>
+                    <Form.Control type="password" placeholder="password" name="password"/>
+                </Form.Group>
+
+                <Form.Group controlId="password2">
+                    <Form.Label>re-enter password</Form.Label>
+                    <Form.Control type="password" placeholder="re-enter password" name="password2"/>
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    sign up
+                </Button>
+            </Form>
+        </Container>
     )
 
 }
