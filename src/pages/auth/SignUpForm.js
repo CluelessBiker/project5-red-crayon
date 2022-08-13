500de6d07187281e7ae2ad4649353ed1dcce4d6a
Lauren-Nicole <popich.red@gmail.com>
2022-08-13 07:25:32 +0000

    Add handleChange function to Signup for form data.

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,8 +1,28 @@
-import React from "react";
-import { Container, Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { Container, Form, Button, Alert } from "react-bootstrap";
 import styles from "../../styles/SignUpForm.module.css";
+import axios from "axios";
 
 const SignUpForm = () => {
+
+    const [signUpData, setSignUpData] = useState({
+        username: "",
+        password: "",
+        password2: "",
+    });
+
+    const { username, password, password2 } = signUpData;
+
+    const history = useHistory();
+
+    const handleChange = (event) => {
+        setSignUpData({
+            ...signUpData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
 
     return (
         <Container className={styles.Container}>
@@ -10,23 +30,46 @@ const SignUpForm = () => {
             <Form>
                 <Form.Group controlId="username">
                     <Form.Label>username</Form.Label>
-                    <Form.Control type="text" placeholder="username" name="username" />
+                    <Form.Control
+                        type="text"
+                        placeholder="username"
+                        name="username"
+                        value={username}
+                        onChange={handleChange}
+                    />
                 </Form.Group>
 
                 <Form.Group controlId="password">
                     <Form.Label>password</Form.Label>
-                    <Form.Control type="password" placeholder="password" name="password"/>
+                    <Form.Control
+                        type="password"
+                        placeholder="password"
+                        name="password"
+                        value={password}
+                        onChange={handleChange}
+                    />
                 </Form.Group>
 
                 <Form.Group controlId="password2">
                     <Form.Label>re-enter password</Form.Label>
-                    <Form.Control type="password" placeholder="re-enter password" name="password2"/>
+                    <Form.Control
+                        type="password"
+                        placeholder="re-enter password"
+                        name="password2"
+                        value={password2}
+                        onChange={handleChange}
+                    />
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
                     sign up
                 </Button>
+
             </Form>
+
+            <div>
+                <Link to="/login">Have an account? Sign in</Link>
+            </div>
         </Container>
     )
 
