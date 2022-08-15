8fcd809692a7edb15ac86fbb81361e4bd0a28c0b
Lauren-Nicole <popich.red@gmail.com>
2022-08-15 07:25:00 +0000

    Insert form fields into create post.

--- src/pages/posts/CreatePostForm.js
+++ src/pages/posts/CreatePostForm.js
@@ -1,8 +1,73 @@
 import React from "react";
+import { Container, Form, Button, Col, Row } from "react-bootstrap";
 
 function CreatePostForm() {
     return (
-        <h1>WHAT?!?!</h1>
+        <Container>
+            <Form>
+                <Form.Group controlId="">
+                    <Form.Label></Form.Label>
+                    <Form.Control type="" placeholder="" />
+                </Form.Group>
+
+                <Form.Group controlId="">
+                    <Form.Label></Form.Label>
+                    <Form.Control type="" placeholder="" />
+                </Form.Group>
+
+                <Form.Group controlId="">
+                    <Form.Label></Form.Label>
+                    <Form.Control type="" placeholder="" />
+                </Form.Group>
+
+                <Form.Group controlId="">
+                    <Form.Label></Form.Label>
+                    <Form.Control type="" placeholder="" />
+                </Form.Group>
+
+                <Row>
+                    <Form.Group as={Col} controlId="formGridState">
+                        <Form.Label></Form.Label>
+                        <Form.Control as="select" defaultValue="Choose...">
+                            <option>Choose...</option>
+                            <option>...</option>
+                        </Form.Control>
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridState">
+                        <Form.Label></Form.Label>
+                        <Form.Control as="select" defaultValue="Choose...">
+                            <option>Choose...</option>
+                            <option>...</option>
+                        </Form.Control>
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridState">
+                        <Form.Label></Form.Label>
+                        <Form.Control as="select" defaultValue="Choose...">
+                            <option>Choose...</option>
+                            <option>...</option>
+                        </Form.Control>
+                    </Form.Group>
+                </Row>
+
+                <Form.Group controlId="">
+                    <Form.Label></Form.Label>
+                    <Form.Control type="" placeholder="" />
+                    <Form.File />
+                </Form.Group>
+                
+                <Row>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                    
+                    <Button variant="primary" type="submit">
+                        Cancel
+                    </Button>
+                </Row>
+            </Form>
+        </Container>
     )
 };
 
