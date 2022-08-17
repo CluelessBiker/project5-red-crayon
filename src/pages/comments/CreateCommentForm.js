d288b73cf588b7acf39874df9dffdd9692eecc78
Lauren-Nicole <popich.red@gmail.com>
2022-08-17 11:06:48 +0000

    Populate comment component.

--- src/pages/comments/CreateCommentForm.js
+++ src/pages/comments/CreateCommentForm.js
@@ -1,6 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, Form, InputGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { axiosRes } from "../../api/axiosDefaults";
+import Avatar from "../../components/Avatar";
 // import styles from "../../styles/CreateCommentForm.module.css";
+import btnStyles from "../../styles/Buttons.module.css";
 
-function CreateCommentForm() {};
+function CreateCommentForm(props) {
+
+    const { post, setPost, setComments, profileImage, profile_id } = props;
+    const [content, setContent] = useState("");
+
+    const handleChange = (event) => {
+        setContent(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+          const { data } = await axiosRes.post("/comments/", {
+            content,
+            post,
+          });
+          setComments((prevComments) => ({
+            ...prevComments,
+            results: [data, ...prevComments.results],
+          }));
+          setPost((prevPost) => ({
+            results: [
+              {
+                ...prevPost.results[0],
+                comments_count: prevPost.results[0].comments_count + 1,
+              },
+            ],
+          }));
+          setContent("");
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
+    return (
+        <Container>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                    <InputGroup>
+                        <Link to={`/profiles/${profile_id}`}>
+                            <Avatar src={profileImage} />
+                        </Link>
+                    </InputGroup>
+
+                    <Form.Control
+                        placeholder="comment"
+                        as="textarea"
+                        value={content}
+                        onChange={handleChange}
+                        rows={2}
+                    />
+
+                    <button
+                        className={btnStyles.Buttons}
+                        disabled={!content.trim()}
+                        type="submit"
+                    >
+                        Post
+                    </button>
+                </Form.Group>
+            </Form>
+        </Container>
+    )
+};
 
 export default CreateCommentForm;
--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -2,16 +2,23 @@ import React, { useEffect, useState } from "react";
 import { Col, Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import CreateCommentForm from "../comments/CreateCommentForm";
 // import postStyles from "../../styles/PostPage.module.css";
 import Post from "./Post";
 
 /**
 * Display single post details & post comments.
+* Function code provided in Moments walkthrough.
 */
 function PostPage() {
 
     const { id } = useParams();
     const [post, setPost] = useState({ results: [] });
+
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [comments, setComments] = useState({ results: [] });
 
     /**
     * Retrieve singular post data.
@@ -37,9 +44,20 @@ function PostPage() {
             <Col>
                 <Post {...post.results[0]} setPosts={setPost} postPage />
             </Col>
-            <Col>
-                <h1>comments</h1>
-            </Col>
+
+            <Container>
+                {currentUser ? (
+                    <CreateCommentForm
+                        profile_id={currentUser.profile_id}
+                        profileImage={profile_image}
+                        post={id}
+                        setPost={setPost}
+                        setComments={setComments}
+                    />
+                ) : comments.results.length ? (
+                    "Comments"
+                ) : null}
+            </Container>
         </Container>
     )
 };
