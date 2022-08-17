b25d14facf3af19059529f12df920c23a74665dd
Lauren-Nicole <popich.red@gmail.com>
2022-08-17 14:23:06 +0000

    Add function to retrieve profiles.

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 
+/**
+* Obtain Profiles from API
+* Return most popular by follower count. 
+* Code provided in Moments walkthrough.
+*/
 const PopularProfiles = () => {
+
+    const [profileData, setProfileData] = useState({
+        pageProfile: { results: [] },
+        popularProfiles: { results: [] },
+    });
+
+    const { popularProfiles } = profileData;
+    const currentUser = useSetCurrentUser();
+
+    /**
+    * Retrieve profiles from API.
+    * Order profiles in descending order by most followed.
+    */
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const { data } = await axiosReq.get("profiles/?ordering=-followers_count");
+                setProfileData((prevState) => ({
+                    ...prevState,
+                    popularProfiles: data,
+                }));
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        handleMount();
+    }, [currentUser]);
 
 
     return (
