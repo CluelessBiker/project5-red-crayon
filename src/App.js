d9b3fd2f3f4260687cd59a168c689ffb145e9a42
Lauren-Nicole <popich.red@gmail.com>
2022-08-18 08:44:00 +0000

    Set variables.

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App() {
             <Route exact path="/posts/create" render={() => <CreatePostForm />} />
             <Route exact path="/posts/:id" render={() => <PostPage />} />
             <Route exact path="/posts/:id/edit" render={() => <EditPostForm />} />
-            <Route exact path="profiles/:id" render={() => <ProfilePage />} />
+            <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
             <Route render={() => <p>Page not found!</p>} />
           </Switch>
         </Col>
--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useProfileData, useSetProfileData } from "../../contexts/ProfileDataContext";
 // import styles from "../../styles.ProfilePage.module.css";
 
 function ProfilePage() {
+
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const currentUser = useCurrentUser();
+    const { id } = useParams();
+    const setProfileData = useSetProfileData();
+    const { pageProfile } = useProfileData();
+    const [profile] = pageProfile.results;
+    const is_owner = currentUser?.username === profile?.owner;
 
     return (
         <h1>Testing....</h1>
