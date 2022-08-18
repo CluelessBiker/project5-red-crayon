6002e836200c4ba764cd7ee91883bf0dd1c49942
Lauren-Nicole <popich.red@gmail.com>
2022-08-18 09:16:09 +0000

    Insert rough profile header.

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
+import Asset from "../../components/Asset";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { useProfileData, useSetProfileData } from "../../contexts/ProfileDataContext";
 // import styles from "../../styles.ProfilePage.module.css";
@@ -21,22 +23,69 @@ function ProfilePage() {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const [{ data: pageProfile }] = await Promise.all([axiosReq.get(`/profiles/${id}/`)]),
+                const [{ data: pageProfile }] = await Promise.all([axiosReq.get(`/profiles/${id}/`)]);
                 
                 setProfileData((prevState) => ({
                     ...prevState,
                     pageProfile: { results: [pageProfile] },
                 }));
 
-                setHadLoaded(true);
+                setHasLoaded(true);
             } catch (err) {};
         };
 
         fetchData();
     }, [id, setProfileData]);
 
+    const profileHeader = (
+        <>
+            <Card>
+                <Row>
+                    <Col>
+                        <Image
+                            roundedCircle
+                            src={profile?.image}
+                        />
+                        { currentUser && !is_owner && (profile?.following_id ? (
+                            <Button>unfollow</Button>
+                        ) : (
+                            <Button>follow</Button>
+                        ))}
+                    </Col>
+
+                    <Col>
+                        <h2>{ profile?.owner }</h2>
+                        <Row>
+                            <Col>
+                                <div>{ profile?.posts_count }</div>
+                                <div>posts</div>
+                            </Col>
+                            <Col>
+                                <div>{ profile?.followers_count }</div>
+                                <div>followers</div>
+                            </Col>
+                            <Col>
+                                <div>{ profile?.following_count }</div>
+                                <div>following</div>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Card>
+        </>
+    );
+
     return (
-        <h1>Testing....</h1>
+        <Container>
+            <h1>Testing....</h1>
+            {hasLoaded ? (
+                <>
+                    { profileHeader }
+                </>
+            ) : (
+                <Asset spinner />
+            )}
+        </Container>
     );
 };
 
