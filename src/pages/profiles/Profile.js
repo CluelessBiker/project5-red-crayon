c860b135ac9141dc04d99f28b70a71f2bf24b89b
Lauren-Nicole <popich.red@gmail.com>
2022-08-18 07:41:21 +0000

    Popolate profile component to display profile image, name, and un/follow button.

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,9 +1,42 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 // import styles from "../../styles/Profile.module.css";
 
 /** 
 * Display profile avatar & un/follow button.
 */
-const Profile = () => {};
+const Profile = (props) => {
+
+    const { profile, imageSize = 35 } = props;
+    const { id, following_id, image, owner } = profile;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
+    return (
+        <div className={`my-3 d-flex align-items-center`}>
+            <div>
+                <Link className={`align-self-center`} to={`/profiles/${id}`}>
+                    <Avatar src={image} height={imageSize} />
+                </Link>
+            </div>
+
+            <div className={`mx-2`}>
+                <strong>{ owner }</strong>
+            </div>
+
+            <div className={`text-right ml-auto`}>
+                {currentUser && !is_owner && (following_id ? (
+                    <Button>unfollow</Button>
+                ) : (
+                    <Button>follow</Button>
+                ))}
+            </div>
+        </div>
+    )
+};
 
 export default Profile;
