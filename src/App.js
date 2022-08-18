21e01f475ce0cba91774a15824c313076b9523d8
Lauren-Nicole <popich.red@gmail.com>
2022-08-18 14:07:05 +0000

    Import & Route EditProfileForm in App

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import PopularProfiles from './pages/profiles/PopularProfiles';
 import ProfilePage from './pages/profiles/ProfilePage';
 import UsernameForm from './pages/profiles/UsernameForm';
 import PasswordForm from './pages/profiles/PasswordForm';
+import EditProfileForm from './pages/profiles/EditProfileForm';
 
 function App() {
 
@@ -57,6 +58,7 @@ function App() {
             <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
             <Route exact path="/profiles/:id/edit/username" render={() => <UsernameForm />} />
             <Route exact path="/profiles/:id/edit/password" render={() => <PasswordForm />} />
+            <Route exact path="/profiles/:id/edit" render={() => <EditProfileForm />} />
             <Route render={() => <p>Page not found!</p>} />
           </Switch>
         </Col>
--- src/pages/profiles/EditProfileForm.js
+++ src/pages/profiles/EditProfileForm.js
@@ -2,6 +2,9 @@ import React from "react";
 
 const EditProfileForm = () => {
 
+    return (
+        <h2>sorry to disappoint</h2>
+    )
 };
 
 export default EditProfileForm;
