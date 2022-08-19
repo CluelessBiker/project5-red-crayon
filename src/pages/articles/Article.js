e6d32fd7a01033c3fc5903e76e935f8cc29f218a
Lauren-Nicole <popich.red@gmail.com>
2022-08-19 09:25:32 +0000

    Create Article component.

---
+++ src/pages/articles/Article.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+/**
+* Render the data of a single article.
+*/
+const Article = (props) => {
+    
+    return (
+        <h1>nothing to report</h1>
+    )
+};
+
+export default Article;
