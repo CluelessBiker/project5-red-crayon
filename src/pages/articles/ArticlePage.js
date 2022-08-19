0c490be73195b26bb7334ff59383911beed3d9c5
Lauren-Nicole <popich.red@gmail.com>
2022-08-19 12:00:28 +0000

    Populate ArticlePage with API request function. Display Article.

--- src/pages/articles/ArticlePage.js
+++ src/pages/articles/ArticlePage.js
@@ -1,11 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Container, Col } from "react-bootstrap";
+import { axiosReq } from "../../api/axiosDefaults";
+import Article from "./Article";
 
 /**
 * Display single article details.
 */
 function ArticlePage() {
 
-    return (
+    const { id } = useParams();
+    const [article, setArticle] = useState({ results: [] });
+    
+    /**
+    * Retrieve article data from API. 
+    */
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const [{ data: article }] = await Promise.all([
+                    axiosReq.get(`/articles/${id}`),
+                ]);
+                setArticle({ results: [article]});
+            } catch (err) {}
+        };
 
+        handleMount();
+    }, [id]);
+
+    return (
+        <Container>
+            <Col>
+                <Article {...article.results[0]} setArticle={setArticle} articlePage />
+            </Col>
+        </Container>
     )
 };
 
