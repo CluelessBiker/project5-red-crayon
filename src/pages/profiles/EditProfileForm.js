3ad39facedcec51b85913377fb5c63a76529d5be
Lauren-Nicole <popich.red@gmail.com>
2022-08-19 06:38:55 +0000

    Add form for edit fields.

--- src/pages/profiles/EditProfileForm.js
+++ src/pages/profiles/EditProfileForm.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import { Alert, Button, Col, Container, Form, Image, Row } from "react-bootstrap";
 import { axiosReq } from "../../api/axiosDefaults";
 import { useCurrentUser, useSetCurrentUser} from "../../contexts/CurrentUserContext";
 
@@ -85,11 +86,145 @@ const EditProfileForm = () => {
         }
     };
 
+    const textFields = (
+        <>
+            <Form.Group>
+                <Form.Label>First name:</Form.Label>
+                <Form.Control
+                    type="text"
+                    name="first_name"
+                    value={first_name}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            {errors?.first_name?.map((message, idx) => (
+                <Alert variant="danger" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+
+            <Form.Group>
+                <Form.Label>Last name:</Form.Label>
+                <Form.Control
+                    type="text"
+                    name="last_name"
+                    value={last_name}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            {errors?.last_name?.map((message, idx) => (
+                <Alert variant="danger" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+
+            <Form.Group>
+                <Form.Label>Nickname:</Form.Label>
+                <Form.Control
+                    type="text"
+                    name="username"
+                    value={username}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            {errors?.username?.map((message, idx) => (
+                <Alert variant="danger" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+
+            <Form.Group>
+                <Form.Label>Country:</Form.Label>
+                <Form.Control
+                    type="text"
+                    name="country"
+                    value={country}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            {errors?.country?.map((message, idx) => (
+                <Alert variant="danger" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+
+            <Form.Group>
+                <Form.Label>About:</Form.Label>
+                <Form.Control
+                    as="textarea"
+                    value={description}
+                    onChange={handleChange}
+                    name="description"
+                    rows={5}
+                />
+            </Form.Group>
+            {errors?.description?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+
+            <Button
+                onClick={() => history.goBack()}
+            >cancel</Button>
+
+            <Button
+                type="submit"
+            >save</Button>
+        </>
+    );
+
     /**
     * Render form to browser. 
     */
     return (
-        <h2>sorry to disappoint</h2>
+        // <h2>ok, now I'm disappointed.</h2>
+        <Form onSubmit={handleSubmit}>
+            <Row>
+                <Col className="py-2 p-0 p-md-2 text-center" md={7} lg={6}>
+                    <Container>
+                        <Form.Group>
+                            {image && (
+                                <figure>
+                                    <Image src={image} fluid />
+                                </figure>
+                            )}
+                            {errors?.image?.map((message, idx) => (
+                                <Alert variant="warning" key={idx}>
+                                {message}
+                                </Alert>
+                            ))}
+
+                            <div>
+                                <Form.Label
+                                    htmlFor="image-upload"
+                                >Change image</Form.Label>
+                            </div>
+                            
+                            <Form.File
+                                id="image-upload"
+                                ref={imageFile}
+                                accept="image/*"
+                                onChange={(e) => {
+                                    if (e.target.files.length) {
+                                        setProfileData({
+                                            ...profileData,
+                                            image: URL.createObjectURL(e.target.files[0]),
+                                        });
+                                    }
+                                }}
+                            />
+                        </Form.Group>
+
+                        <div className="d-md-none">{textFields}</div>
+                    </Container>
+                </Col>
+
+                <Col md={5} lg={6} className="d-none d-md-block p-0 p-md-2 text-center">
+                    <Container>{textFields}</Container>
+                </Col>
+            </Row>
+        </Form>
     );
 };
 
