725ce490c85a8e762003929a260d42f865e9561b
Lauren-Nicole <popich.red@gmail.com>
2022-08-19 12:20:30 +0000

    Build API request function.

--- src/pages/articles/ArticlesPage.js
+++ src/pages/articles/ArticlesPage.js
@@ -1,6 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
-function ArticlesPage() {
+/**
+* Display all articles.
+*/
+function ArticlesPage({ message, filter="" }) {
+
+    const [articles, setArticles] = useState({ results: [] });
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const { pathname } = useLocation();
+
+    const [query, setQuery] = useState("");
+
+    /**
+    * Retrieve articles from API
+    * Display spinner until content has loaded.
+    * Delay search filter from making API requests with each keystroke.
+    */
+    useEffect(() => {
+        const fetchArticles = async () => {
+            try {
+                const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
+                setArticles(data);
+                setHasLoaded(true);
+            } catch (err) {}
+        };
+
+        setHasLoaded(false);
+
+        const timer = setTimeout(() => {
+            fetchArticles();
+        }, 1000);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [filter, query, pathname]);
 
     return (
         <h1>News</h1>
