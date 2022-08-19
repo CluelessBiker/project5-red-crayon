e31dfa1303fdedd16109a8e630fe70297b34ac24
Lauren-Nicole <popich.red@gmail.com>
2022-08-19 14:45:20 +0000

    Resolve console errors. Update render in index.js.

--- src/index.js
+++ src/index.js
@@ -6,18 +6,32 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { CurrentUserProvider } from './contexts/CurrentUserContext';
 import { ProfileDataProvider } from './contexts/ProfileDataContext';
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.render(
-  // <React.StrictMode>
-    <Router>
-      <CurrentUserProvider>
-        <ProfileDataProvider>
-          <App />
-        </ProfileDataProvider>
-      </CurrentUserProvider>
-    </Router>,
-  // </React.StrictMode>,
-  document.getElementById('root')
+// ReactDOM.render(
+//   // <React.StrictMode>
+//     <Router>
+//       <CurrentUserProvider>
+//         <ProfileDataProvider>
+//           <App />
+//         </ProfileDataProvider>
+//       </CurrentUserProvider>
+//     </Router>,
+//   // </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <Router>
+    <CurrentUserProvider>
+      <ProfileDataProvider>
+        <App />
+      </ProfileDataProvider>
+    </CurrentUserProvider>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
