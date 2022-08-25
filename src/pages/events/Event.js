455d7200663d011eecf9e4f3a8c74eba87198239
Lauren-Nicole <popich.red@gmail.com>
2022-08-25 11:51:13 +0000

    Add functions to Event component to edit/delete item.

--- src/pages/events/Event.js
+++ src/pages/events/Event.js
@@ -1,9 +1,56 @@
 import React from "react";
+import { Col, Container, Media, Row } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import { axiosRes } from "../../api/axiosDefaults";
+import Avatar from "../../components/Avatar";
+import DropdownMenu from "../../components/DropdownMenu";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const Event = (props) => {
 
+    const {
+        id,
+        owner,
+        profile_id,
+        profile_image,
+        created_on,
+        modified_on,
+        title,
+        content,
+        date,
+        time,
+        city,
+        country,
+        price,
+        event_link,
+        eventPage,
+    } = props;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+    const history = useHistory;
+
+    /**
+    * Route user to Edit page for Event 
+    */
+    const handleEdit = () => {
+        history.push(`/events/${id}/edit`);
+    };
+
+    /**
+    * Delete Event from API. 
+    */
+    const handleDelete = async () => {
+        try {
+            await axiosRes.delete(`/events/${id}/`);
+            history.goBack();
+        } catch (err) {
+            // console.log(err);
+        }
+    };
+
     return (
-        <h1>I'm an event.</h1>
+
     );
 };
 
