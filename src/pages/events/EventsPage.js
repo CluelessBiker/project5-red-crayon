dfa0cf4d3fe3962954801c56e7be3abd9dee231d
Lauren-Nicole <popich.red@gmail.com>
2022-08-25 12:13:00 +0000

    Create EventsPage component.

---
+++ src/pages/events/EventsPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function EventsPage() {
+
+    return (
+        <Container>
+            <br />
+            <h2>Browse upcoming events!</h2>
+        </Container>
+    );
+};
+
+export default EventsPage;
