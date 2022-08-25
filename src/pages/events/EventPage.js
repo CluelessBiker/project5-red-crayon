1f6d2fa18cafa9e56cd1665a445af45956ea14a1
Lauren-Nicole <popich.red@gmail.com>
2022-08-25 12:03:57 +0000

    Insert function to retrieve event data from API. Render event component.

--- src/pages/events/EventPage.js
+++ src/pages/events/EventPage.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import Event from "./Event";
 
+/**
+* Display content of a single Event.
+*/
 function EventPage() {
+
+    const { id } = useParams();
+    const [event, setEvent] = useState({ results: [] });
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const [{ data: event }] = await Promise.all([
+                    axiosReq.get(`/events/${id}`),
+                ]);
+                setEvent({ results: [event] });
+            } catch (err) {
+                // console.log(err);
+            };
+        };
+
+        handleMount();
+    }, [id]);
 
     return (
         <Container>
-            <h1>EventPage</h1>
+            <Event {...event.results[0]} setEvent={setEvent} eventPage />
         </Container>
     );
 };
