5204a8c1608a701cc13444126fb8c76dea1889a7
Lauren-Nicole <popich.red@gmail.com>
2022-08-25 13:45:37 +0000

    Create About component.

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function About() {
+
+    return (
+        <Container>
+            <h2>About us:</h2>
+        </Container>
+    );
+};
+
+export default About;
