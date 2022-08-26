55e2dfa51cd321aa2068b66d684b59b1d1923feb
Lauren-Nicole <popich.red@gmail.com>
2022-08-26 12:25:07 +0000

    Address eslint errors in NotFound component.

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import styles from "../styles/NotFound.module.css";
-import FourOFour from "../assets/notfound.png";
 import { Container } from 'react-bootstrap';
+import styles from '../styles/NotFound.module.css';
+import FourOFour from '../assets/notfound.png';
 
 /**
-* Display error graphic for non-existent pages.
-*/
+ * Display error graphic for non-existent pages.
+ */
 const NotFound = () => {
-
-    return (
-        <Container>
-            <div className={styles.ErrorGraphic}>
-                <img src={FourOFour} alt="404 Error. Page not found."/>
-            </div>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <div className={styles.ErrorGraphic}>
+        <img src={FourOFour} alt="404 Error. Page not found." />
+      </div>
+    </Container>
+  );
+};
 
 export default NotFound;
